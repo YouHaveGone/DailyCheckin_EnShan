06c08218e6e5f6a001d7b6335fe03bf9864aa62f
BNDou <67632799+BNDou@users.noreply.github.com>
2022-10-30 21:27:40 +0800

    dailyCheckin_EnShan

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
+<!--
+ * @Author: BNDou
+ * @Date: 2022-10-30 19:12:57
+ * @LastEditTime: 2022-10-30 20:57:22
+ * @FilePath: \DailyCheckin_EnShan\README.md
+ * @Description: 
+-->
+
 # Dailylogin_EnShan
- 恩山无线论坛每日签到
+恩山无线论坛每日签到
+
+### 青龙部署
+
+1、把源码新建到/ql/scripts/
+
+2、环境变量添加”COOKIE_ENSHAN“，值填入cookie
+
+3、任务命令
+```text
+task dailyCheckinEnShan.py
+```
---
+++ dailyCheckinEnShan.py
@@ -0,0 +1,71 @@
+'''
+Author: BNDou
+Date: 2022-10-30 19:17:34
+LastEditTime: 2022-10-30 20:53:09
+FilePath: \DailyCheckin_EnShan\dailyCheckinEnShan.py
+Description: Fork from leeairw/Enshan
+'''
+
+import requests, json, time, os, sys
+sys.path.append('.')
+requests.packages.urllib3.disable_warnings()
+try:
+    from pusher import pusher
+except:
+    pass
+from lxml import etree
+
+cookie = os.environ.get("COOKIE_ENSHAN")
+
+def run(*arg):
+    msg = ""
+    s = requests.Session()
+    s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'})
+
+    # 签到
+    url = "https://www.right.com.cn/forum/home.php?mod=spacecp&ac=credit&op=log&suboperation=creditrulelog"
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding' : 'gzip, deflate, br',
+        'Accept-Language' : 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Connection' : 'keep-alive',
+        'Cookie': cookie,
+        'Host' : 'www.right.com.cn',
+        'Upgrade-Insecure-Requests' : '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
+    }
+    try:
+        r = s.get(url, headers=headers, timeout=120)
+        # print(r.text)
+        if '每天登录' in r.text:
+            h = etree.HTML(r.text)
+            data = h.xpath('//tr/td[6]/text()')
+            msg += f'签到成功或今日已签到，最后签到时间：{data[0]}'
+        else:
+            msg += '签到失败，可能是cookie失效了！'
+            pusher(msg)
+    except:
+        msg = '无法正常连接到网站，请尝试改变网络环境，试下本地能不能跑脚本，或者换几个时间点执行脚本'
+    return msg + '\n'
+
+def main(*arg):
+    msg = ""
+    global cookie
+    if "\\n" in cookie:
+        clist = cookie.split("\\n")
+    else:
+        clist = cookie.split("\n")
+    i = 0
+    while i < len(clist):
+        msg += f"第 {i+1} 个账号开始执行任务\n"
+        cookie = clist[i]
+        msg += run(cookie)
+        i += 1
+    print(msg[:-1])
+    return msg[:-1]
+
+if __name__ == "__main__":
+    if cookie:
+        print("----------恩山论坛开始尝试签到----------")
+        main()
+        print("----------恩山论坛签到执行完毕----------")
